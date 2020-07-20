832b88f72a9a7761096f4cca61e82e666774046c
Aaditya chapagain <chapagain.aaditya@gmail.com>
2020-07-20 15:31:21 +0545

    redirect from overworld to botworld

--- parlai/chat_service/services/websocket/run.py
+++ parlai/chat_service/services/websocket/run.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python3
-
-# Copyright (c) Facebook, Inc. and its affiliates.
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
 """
 Websocket Runner.
 """
@@ -12,7 +7,6 @@ import parlai.chat_service.utils.config as config_utils
 
 
 SERVICE_NAME = 'websocket'
-
 
 def setup_args():
     """

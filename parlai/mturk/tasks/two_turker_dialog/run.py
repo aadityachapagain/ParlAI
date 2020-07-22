b977a9f9202d18c1e8377634e87ce3920116fd28
Sagar Sapkota <spkt.sagar@gmail.com>
2020-07-22 19:36:59 +0545

    Include qualification flow in two turker dialog mturk task(need to create good UI)

--- parlai/mturk/tasks/two_turker_dialog/run.py
+++ parlai/mturk/tasks/two_turker_dialog/run.py
@@ -1,6 +1,7 @@
 import os
 
 from parlai.core.params import ParlaiParser
+from parlai.mturk.core import mturk_utils
 from parlai.mturk.tasks.two_turker_dialog.worlds import (
     TwoTurkerDialogOnboardWorld,
     TwoTurkerDialogWorld
@@ -22,8 +23,31 @@ def main():
     mturk_manager = MTurkManager(opt=opt, mturk_agent_ids=mturk_agent_ids, use_db=True)
     mturk_manager.setup_server()
 
+    qual_pass_name = 'ChildCompanionDialogQualificationPass'
+    qual_pass_desc = (
+        'Qualification for a worker correctly completing the '
+        'child companion dialog qualification test task.'
+    )
+    pass_qual_id = mturk_utils.find_or_create_qualification(
+        qual_pass_name, qual_pass_desc, opt['is_sandbox']
+    )
+    print('Created pass qualification: ', pass_qual_id)
+
+    qual_fail_name = 'ChildCompanionDialogQualificationFail'
+    qual_fail_desc = (
+        'Qualification for a worker not correctly completing the '
+        'child companion dialog qualification test task.'
+    )
+    fail_qual_id = mturk_utils.find_or_create_qualification(
+        qual_fail_name, qual_fail_desc, opt['is_sandbox']
+    )
+    print('Created fail qualification: ', fail_qual_id)
+
     def run_onboard(worker):
-        world = TwoTurkerDialogOnboardWorld(opt=opt, mturk_agent=worker)
+        world = TwoTurkerDialogOnboardWorld(opt=opt,
+                                            mturk_agent=worker,
+                                            pass_qual_id=pass_qual_id,
+                                            fail_qual_id=fail_qual_id)
         while not world.episode_done():
             world.parley()
         world.shutdown()
@@ -33,7 +57,15 @@ def main():
     try:
         mturk_manager.start_new_run()
         mturk_manager.ready_to_accept_workers()
-        mturk_manager.create_hits()
+
+        agent_qualifications = [
+            {
+                'QualificationTypeId': fail_qual_id,
+                'Comparator': 'DoesNotExist',
+                'ActionsGuarded': 'Accept',
+            }
+        ]
+        mturk_manager.create_hits(qualifications=agent_qualifications)
 
         def check_workers_eligibility(workers):
             return workers
@@ -64,6 +96,8 @@ def main():
     except BaseException:
         raise
     finally:
+        mturk_utils.delete_qualification(pass_qual_id, opt['is_sandbox'])
+        mturk_utils.delete_qualification(fail_qual_id, opt['is_sandbox'])
         mturk_manager.expire_all_unassigned_hits()
         mturk_manager.shutdown()
 
--- parlai/mturk/tasks/two_turker_dialog/worlds.py
+++ parlai/mturk/tasks/two_turker_dialog/worlds.py
@@ -1,21 +1,78 @@
 from joblib import Parallel, delayed
 from parlai.core.worlds import validate
+from parlai.mturk.core import mturk_utils
 from parlai.mturk.core.worlds import MTurkOnboardWorld, MTurkTaskWorld
 
 
 class TwoTurkerDialogOnboardWorld(MTurkOnboardWorld):
+    def __init__(self, opt, mturk_agent, pass_qual_id, fail_qual_id):
+        super(TwoTurkerDialogOnboardWorld, self).__init__(opt, mturk_agent)
+        self.opt = opt
+        self.pass_qual_id = pass_qual_id
+        self.fail_qual_id = fail_qual_id
+        self.first_time = not mturk_utils.check_worker_qualification_exist(
+            self.pass_qual_id,
+            self.mturk_agent.worker_id,
+            is_sandbox=self.opt['is_sandbox']
+        )
+        self.pass_qual_test = False if self.first_time else True
 
     def parley(self):
-        self.mturk_agent.observe({
-            'id': 'SYSTEM',
-            'text': (
-                'Welcome! If you are ready,'
-                'please click "I am ready, continue" to start this task.'
-            ),
-            # This is message(Can be formated in html) that can be shown in onboarding page
-        })
-        self.mturk_agent.act()
+        if self.first_time:
+            # TODO Present worker the qualification task
+            self.mturk_agent.observe({
+                'id': 'SYSTEM',
+                'text': (
+                    'You\'re here for the first time. We need you to complete a '
+                    'qualification test before you proceed to the main task.'
+                )
+            })
+            self.mturk_agent.observe({
+                'id': 'SYSTEM',
+                'text': 'What is 1 + 1? \n Please give an integer result.'
+            })
+            agent_act = self.mturk_agent.act()
+            if int(agent_act['text']) == 2:
+                self.pass_qual_test = True
+                self.mturk_agent.observe({
+                    'id': 'SYSTEM',
+                    'text': 'Congratulations you completed qualification task. You may proceed to main task'
+                })
+            else:
+                self.pass_qual_test = False
+                self.mturk_agent.observe({
+                    'id': 'SYSTEM',
+                    'text': 'Sorry you\'ve failed our qualification test task. You cannot proceed to main task.'
+                })
+        else:
+            # Worker is good to go to main task
+            self.mturk_agent.observe({
+                'id': 'SYSTEM',
+                'text': (
+                    'Welcome back! You\'ve already completed our qualification task. '
+                    'You\'re good to go. If you are ready, '
+                    'Please send anything to continue.'
+                ),
+            })
+            self.mturk_agent.act()
         self.episodeDone = True
+
+    def shutdown(self):
+        if self.pass_qual_test:
+            if self.first_time:
+                mturk_utils.give_worker_qualification(
+                    self.mturk_agent.worker_id,
+                    self.pass_qual_id,
+                    is_sandbox=self.opt['is_sandbox']
+                )
+        else:
+            mturk_utils.give_worker_qualification(
+                self.mturk_agent.worker_id,
+                self.fail_qual_id,
+                is_sandbox=self.opt['is_sandbox']
+            )
+            self.mturk_agent.shutdown()
+            self.mturk_agent.reject_work(reason="Turker failed the qualification task")
 
 
 class TwoTurkerDialogWorld(MTurkTaskWorld):
@@ -78,6 +135,7 @@ class TwoTurkerDialogWorld(MTurkTaskWorld):
         )
 
     def review_work(self):
+        # TODO need to disable auto approve if manual review is done
         global review_agent
 
         def review_agent(ag):

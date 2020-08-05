49bb35eb032f034d748c77f88642d8746aced16b
Sagar Sapkota <spkt.sagar@gmail.com>
2020-08-05 22:08:28 +0545

    Log BOT Response time and number of API error encountered

--- parlai/mturk/tasks/two_turker_dialog_fallback_bot/api_bot_agent.py
+++ parlai/mturk/tasks/two_turker_dialog_fallback_bot/api_bot_agent.py
@@ -1,4 +1,6 @@
+import os
 import queue
+import time
 import json
 import requests
 from parlai.core.agents import Agent
@@ -11,18 +13,25 @@ class APIBotAgent(Agent):
         self.bot_url = f'http://{self.opt["bot_host"]}:{str(self.opt["bot_port"])}'
         self.session_id = None
         self.resp_queue = queue.Queue()
+        self.bot_response_time = []
+        self.bot_request_error_counter = 0
 
     def observe(self, observation):
         if self.session_id:
             observation.update({'session_id': self.session_id})
-        response = requests.post(f'{self.bot_url}/interact',
-                                 json=observation,
-                                 auth=(self.opt['bot_username'],
-                                       self.opt['bot_password']))
-        response = json.loads(response.content)
-        if "session_id" in response:
-            self.session_id = response["session_id"]
-        self.resp_queue.put(response)
+        try:
+            t = time.time()
+            response = requests.post(f'{self.bot_url}/interact',
+                                     json=observation,
+                                     auth=(self.opt['bot_username'],
+                                           self.opt['bot_password']))
+            self.bot_response_time.append(time.time() - t)
+            response = json.loads(response.content)
+            if "session_id" in response:
+                self.session_id = response["session_id"]
+            self.resp_queue.put(response)
+        except Exception as e:
+            self.bot_request_error_counter += 1
 
     def act(self):
         result = self.resp_queue.get()
@@ -34,6 +43,16 @@ class APIBotAgent(Agent):
 
     def shutdown(self):
         super(APIBotAgent, self).shutdown()
+        time_log_file = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+                                     'run_data', 'bot_response_time.txt')
+        error_counter_file = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+                                          'run_data', 'bot_error_counter.txt')
+        with open(time_log_file, 'a') as f:
+            f.write(str(self.bot_response_time) + '\n')
+
+        with open(error_counter_file, 'a') as f:
+            f.write(str(self.bot_request_error_counter) + '\n')
+
         _ = requests.post(f'{self.bot_url}/interact',
                           json={'text': '[DONE]'},
                           auth=(self.opt['bot_username'],

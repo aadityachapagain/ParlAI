c015716a65b7fb90540a2a791e68b28d9375a86b
Sagar Sapkota <spkt.sagar@gmail.com>
2020-08-24 22:06:23 +0545

    config for 2nd golden 50 run

--- parlai/mturk/tasks/child_dialog/dedicated_workers.py
+++ parlai/mturk/tasks/child_dialog/dedicated_workers.py
@@ -47,6 +47,6 @@ class ReviewGSheet:
 
     def get_golden_workers_list(self, link: str = None, sheets_num: int = None):
         review_df = self.convert_gsheets_to_dataframe(link, sheets_num)
-        golden_workers = review_df[review_df['Points'] == 'Golden 50'][
-            'Worker ID'].dropna().unique().tolist()
-        return golden_workers
+        all_golden_50 = review_df[(review_df['Points'] == 'Golden 50') & (review_df['DATE'] == '8/24/2020')]
+        worker_golden50_count = all_golden_50.groupby('Worker ID')['Points'].agg('count')
+        return list(worker_golden50_count[worker_golden50_count > 2].index)

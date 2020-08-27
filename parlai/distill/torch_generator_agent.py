86318c43e495650084c69d2951cb50296d81792f
Aaditya chapagain <chapagain.aaditya@gmail.com>
2020-08-27 22:13:53 +0545

    bug fixs #4

--- parlai/distill/torch_generator_agent.py
+++ parlai/distill/torch_generator_agent.py
@@ -89,6 +89,7 @@ class TorchDistillGeneratorAgent(TorchGeneratorAgent):
         """
         Add command line arguments.
         """
+        agent = argparser.add_argument_group('Distill generator Agent')
         super(TorchGeneratorAgent, cls).add_cmdline_args(argparser)
         return agent
 

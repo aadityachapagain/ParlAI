420ffc4914d7b5d1300295490d47ed880cc89c11
Sagar Sapkota <spkt.sagar@gmail.com>
2020-09-07 14:30:53 +0545

    CCD mturk to parlai task including new conv themes

--- parlai/tasks/child_companion_dialog/build.py
+++ parlai/tasks/child_companion_dialog/build.py
@@ -4,21 +4,40 @@ import json
 
 from parlai.core import build_data
 
-
-RESOURCES = [
-    build_data.DownloadableFile(
-        '1vH5ScrD2lUVpAi7N_KkrG0ZvvztYnYrI',
-        'child_companion_dialog.zip',
-        '3771e18a4213e941fa1f67c5ada4443781b8162b1437890349be7ae626ec5374',
-        zipped=True,
-        from_google=True
-    )
-]
+RESOURCES = {
+    'All': [
+        build_data.DownloadableFile(
+            '1vH5ScrD2lUVpAi7N_KkrG0ZvvztYnYrI',
+            'child_companion_dialog.zip',
+            '3771e18a4213e941fa1f67c5ada4443781b8162b1437890349be7ae626ec5374',
+            zipped=True,
+            from_google=True
+        ),
+    ],
+    '3B': [
+        build_data.DownloadableFile(
+            '1ZLjOH52FmQpdTn_OOD6oKHHOkw2U30MG',
+            'ccd_3B.zip',
+            '184bbd8b7b3cc15f5b6144062db48777b2fa7e97a8d213814f22bd8d7a75392d',
+            zipped=True,
+            from_google=True
+        ),
+    ],
+    '90M': [
+        build_data.DownloadableFile(
+            '1i6s-UxxR85-5mC6UJ3j-ZbR7T8FJdffY',
+            'ccd_90M.zip',
+            '079881dd2c73502440b2fed35939dc238e842545e31ccd7f155f919d6450c827',
+            zipped=True,
+            from_google=True
+        ),
+    ],
+}
 
 
 def build(opt):
     dpath = os.path.join(opt['datapath'], 'child_companion_dialog')
-    version = 'v0.0'
+    version = opt.get('task_data_version', 'v0.0')
 
     if not build_data.built(dpath, version_string=version):
         print('[building data: ' + dpath + ']')
@@ -28,7 +47,7 @@ def build(opt):
         build_data.make_dir(dpath)
 
         # Download the data.
-        for downloadable_file in RESOURCES:
+        for downloadable_file in RESOURCES.get(version, RESOURCES['All']):
             downloadable_file.download_file(dpath)
 
         _create_parlai_format(dpath)
@@ -64,6 +83,20 @@ def _get_lines(conv):
         if conv['bot_persona'].strip() != '':
             persona_text = 'your persona: ' + conv['bot_persona']
             lines[0]['text'] = persona_text + '\n' + lines[0]['text']
+    elif ('context' in conv) and lines:
+        persona_sentences = []
+        if conv['context'].get('conv_theme') and conv['context'].get('conv_theme').get('theme_sentence'):
+            persona_sentences.append('your persona: ' + conv['context']['conv_theme']['theme_sentence'])
+
+        if conv['context'].get('personas') and conv.get('bot_role'):
+            persona_sentences.append('your persona: ' + (conv['context']['personas']['robot_persona']
+                                                         if conv.get('bot_role') == 'KARU'
+                                                         else conv['context']['personas']['child_persona']))
+        elif conv['context'].get('personas') and conv.get('worker_role'):
+            persona_sentences.append('your persona: ' + (conv['context']['personas']['robot_persona']
+                                                         if conv.get('worker_role') == 'CHILD'
+                                                         else conv['context']['personas']['child_persona']))
+        lines[0]['text'] = '\n'.join(persona_sentences) + '\n' + lines[0]['text']
     else:
         lines = []
 

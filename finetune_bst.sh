7e78fa7c8ef75b9488ce6e9c518ca5494e7bcbb0
Aaditya chapagain <chapagain.aaditya@gmail.com>
2020-09-08 17:12:29 +0545

    add dictionary file

--- finetune_bst.sh
+++ finetune_bst.sh
@@ -2,6 +2,7 @@ export GOOGLE_APPLICATION_CREDENTIALS="gcp/fusemachineschat.json"
 export PYTHONFAULTHANDLER=1
 python parlai/scripts/train_model.py \
 -t blended_skill_talk,wizard_of_wikipedia,convai2:normalized \
+--run-tag 90m_finetune_bst \
 -m transformer/generator --multitask-weights 1,3,3,3 \
 --embedding-size 512 --n-layers 8 --ffn-size 2048 \
 --dropout 0.1 --n-heads 16 --learn-positional-embeddings True \
@@ -14,8 +15,10 @@ python parlai/scripts/train_model.py \
 --skip-generation True -vp 15 -stim 60 \
 -vme 20000 -bs 16 -vmt ppl -vmm min \
 --save-after-valid True \
+--bpe-vocab "karu_bot_90M.checkpoint.dict-vocab.json" \
+--bpe-merge "karu_bot_90M.checkpoint.dict-merges.txt" \
 --delimiter '  ' \
 --fp16-impl apex \
 --dynamic-batching full --batchsize 32 \
 --model-file data/models/Karu/karu_bot_90M \
---init-model data/models/Karu/karu_bot_90M.checkpoint \
+--init-model data/models/Karu/karu_bot_90M.checkpoint

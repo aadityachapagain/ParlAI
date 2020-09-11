0e1d4200117ee73d2a6d8e8a3d5853c09956c18d
Aaditya chapagain <chapagain.aaditya@gmail.com>
2020-09-11 16:47:00 +0545

    train with new params

--- pretrain_2.sh
+++ pretrain_2.sh
@@ -7,24 +7,26 @@ python3 parlai/tasks/reddit_datasets/build.py
 python parlai/scripts/multiprocessing_train.py \
 -t reddit_datasets \
 -dt train:stream \
---run-tag "90m_pretrain_prelayernorm1" \
+--run-tag "90m_pretrain_2" \
 --wand-project-name "Karu_chatbot_v0" \
 --wand-run-name "90M Model Pretraining" \
---wand-id "90MmodelPretrainingpre-sug-by-roller-0" \
+--wand-id "90MmodelPretraining-diff-enc-dec-layer" \
 --wandb-notes "trying hypterparamter suggested by stephen but with 90m model" \
 -m transformer/generator \
 --load-from-checkpoint True \
---embedding-size 512 --n-layers 8 --ffn-size 2048 \
+--embedding-size 768 \
+--n-encoder-layers 2 --n-decoder-layers 14 \
+--ffn-size 2048 \
 --dropout 0.1 --n-heads 16 --learn-positional-embeddings True \
 --n-positions 512 --variant prelayernorm \
 --activation gelu --skip-generation True --fp16 True \
 --text-truncate 256 --label-truncate 128 --dict-tokenizer bytelevelbpe \
--lr 2e-05 --optimizer adamax --lr-scheduler cosine --max-lr-steps 800000 \
+-lr 3e-04 --optimizer adam --lr-scheduler cosine --max-lr-steps 800000 \
 --lr-scheduler-patience 3 \
---warmup_updates 6125 \
---gradient-clip 10.0 -veps 0.5 --betas 0.9,0.999 --update-freq 2 \
+--warmup_updates 2000 \
+--gradient-clip 10.0 --betas 0.9,0.999 --update-freq 2 \
 --log_every_n_secs 30 \
---attention-dropout 0.0 --relu-dropout 0.0 \
+--attention-dropout 0.1 --relu-dropout 0.0 \
 --history-add-global-end-token end \
 --skip-generation True \
 -vme 20000 -vmt ppl -vmm min \
@@ -33,11 +35,11 @@ python parlai/scripts/multiprocessing_train.py \
 --dict-file zoo:blender/blender_3B/model.dict \
 --delimiter '  ' \
 --fp16-impl apex \
---dynamic-batching full --batchsize 56 --eval-batchsize 60 \
+--dynamic-batching full --batchsize 4 --eval-batchsize 8 \
 --model-file /tmp/models/Karu/karu_bot_90M \
 --init-model /tmp/models/Karu/karu_bot_90M.checkpoint \
 -tblog True \
---validation-patience 10 \
+-vp 3  -veps 0.25 \
 --inference beam  --beam-size 10 --beam-block-ngram 3 \
 --beam-context-block-ngram 3 \
 --beam-min-length 20 \

182826307ca8c519e799c56369dbac8d68ea13d0
aadityachapagain <chapagain.aaditya@gmail.com>
2020-10-01 11:08:43 +0545

    talk with bot 1

--- talk_with_bot.sh
+++ talk_with_bot.sh
@@ -1,5 +1,5 @@
 python parlai/core/latest_checkpoint.py --model-file /tmp/Karu/Karu_M1.checkpoint --run-tag "M1_finetune"
 
-python parlai/scripts/interactive.py /
---model-file /tmp/Karu/Karu_M1.checkpoint /
+python parlai/scripts/interactive.py \
+--model-file /tmp/Karu/Karu_M1.checkpoint \
 --model transformer/generator
